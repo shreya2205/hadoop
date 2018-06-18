c2d093c8e96b1ed6b738d5f5e34b3fc4ccf4a531
Shreya Gupta <shreya.gupta2205@gmail.com>
2018-06-18 17:19:31 +0530

    Add basic functionalities

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/coder/ClayCodeErasureDecodingStep.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/coder/ClayCodeErasureDecodingStep.java
@@ -10,109 +10,109 @@ import java.util.Map;
 
 public class ClayCodeErasureDecodingStep implements ErasureCodingStep {
 
-    private ECBlock[] inputBlocks;
-    private ECBlock[] outputBlocks;
-    private int[] erasedIndexes;
-    private RawErasureDecoder pairWiseDecoder;
-    private RawErasureDecoder rsRawDecoder;
-    private final int SUB_PACKETIZATION;
+  private ECBlock[] inputBlocks;
+  private ECBlock[] outputBlocks;
+  private int[] erasedIndexes;
+  private RawErasureDecoder pairWiseDecoder;
+  private RawErasureDecoder rsRawDecoder;
+  private final int SUB_PACKETIZATION;
 
-    /**
-     * Basic constructor with necessary info
-     * @param inputs Blocks to encode
-     * @param outputs Blocks to decode
-     * @param pairWiseDecoder Decoder for the pair wise transforms
-     * @param rsRawDecoder Decoder for each layer
-     */
-    public ClayCodeErasureDecodingStep(ECBlock[] inputs,
-                                       int[] erasedIndexes,
-                                       ECBlock[] outputs,
-                                       RawErasureDecoder pairWiseDecoder,
-                                       RawErasureDecoder rsRawDecoder,
-                                       int SUB_PACKETIZATION) {
-        this.inputBlocks = inputs;
-        this.erasedIndexes = erasedIndexes;
-        this.outputBlocks = outputs;
-        this.rsRawDecoder = rsRawDecoder;
-        this.pairWiseDecoder = pairWiseDecoder;
-        this.SUB_PACKETIZATION = SUB_PACKETIZATION;
+  /**
+   * Basic constructor with necessary info
+   * @param inputs Blocks to encode
+   * @param outputs Blocks to decode
+   * @param pairWiseDecoder Decoder for the pair wise transforms
+   * @param rsRawDecoder Decoder for each layer
+   */
+  public ClayCodeErasureDecodingStep(ECBlock[] inputs,
+                                     int[] erasedIndexes,
+                                     ECBlock[] outputs,
+                                     RawErasureDecoder pairWiseDecoder,
+                                     RawErasureDecoder rsRawDecoder,
+                                     int SUB_PACKETIZATION) {
+    this.inputBlocks = inputs;
+    this.erasedIndexes = erasedIndexes;
+    this.outputBlocks = outputs;
+    this.rsRawDecoder = rsRawDecoder;
+    this.pairWiseDecoder = pairWiseDecoder;
+    this.SUB_PACKETIZATION = SUB_PACKETIZATION;
 
-    }
+  }
 
-    /**
-     * @param z_vector plane index in vector form
-     * @return z plane index in integer form
-     */
-    public int getZ(int[] z_vector) {}
+  /**
+   * @param z_vector plane index in vector form
+   * @return z plane index in integer form
+   */
+  public int getZ(int[] z_vector) {}
 
-    /**
-     * @param z plane index in integer form
-     * @return plane index in vector form
-     */
-    public int[] getZVector(int z) {}
+  /**
+   * @param z plane index in integer form
+   * @return plane index in vector form
+   */
+  public int[] getZVector(int z) {}
 
-    /**
-     * @param z_vector plane in vector form
-     * @return intersection score of the plane
-     */
-    public int getIntersectionScore(int[] z_vector) {}
+  /**
+   * @param z_vector plane in vector form
+   * @return intersection score of the plane
+   */
+  public int getIntersectionScore(int[] z_vector) {}
 
-    /**
-     * @param z plane index in integer form
-     * @return map of intersection score and z.
-     *
-     * For each intersection score finds out all the planes whose intersection score = z.
-     */
-    public Map<int , int[]> getAllIntersectionScore(int z) {}
+  /**
+   * @param z plane index in integer form
+   * @return map of intersection score and z.
+   *
+   * For each intersection score finds out all the planes whose intersection score = z.
+   */
+  public Map<int , int[]> getAllIntersectionScore(int z) {}
 
-    /**
-     * @param x x coordinate of the node in plane
-     * @param y y coordinate of the node in plane
-     * @return x+y*q
-     */
-    public  int getIndexInPlane(int x, int y) {}
-
-
-    /**
-     * @param indexInPlane index of the node in the plane (x+y*q)
-     * @param z_vector plane index in vector form
-     * @return return the error type for a node in a plane.
-     * Error types possible are : {0,1,2}
-     */
-    public int getErrorType(int indexInPlane, int[] z_vector) {}
-
-    /**
-     * @param z_vector plane index in vector form
-     * @param temp temporary array which stores decoupled values
-     * @return decoupled values for all non-null nodes
-     */
-    public ByteBuffer[] getDecoupledPlane(int z_vector, ByteBuffer[] temp[]) {}
+  /**
+   * @param x x coordinate of the node in plane
+   * @param y y coordinate of the node in plane
+   * @return x+y*q
+   */
+  public  int getIndexInPlane(int x, int y) {}
 
 
-    /**
-     * @param input1 first input for matrix
-     * @param input2 second input for matrix
-     * @return two remaining outputs using matrix
-     */
-    public int[] getPairWiseCouple(int input1, int input2) {}
+  /**
+   * @param indexInPlane index of the node in the plane (x+y*q)
+   * @param z_vector plane index in vector form
+   * @return return the error type for a node in a plane.
+   * Error types possible are : {0,1,2}
+   */
+  public int getErrorType(int indexInPlane, int[] z_vector) {}
 
-    @Override
-    public ECBlock[] getInputBlocks() {
-        return inputBlocks;
-    }
+  /**
+   * @param z_vector plane index in vector form
+   * @param temp temporary array which stores decoupled values
+   * @return decoupled values for all non-null nodes
+   */
+  public ByteBuffer[] getDecoupledPlane(int z_vector, ByteBuffer[] temp[]) {}
 
-    @Override
-    public ECBlock[] getOutputBlocks() {
-        return outputBlocks;
-    }
 
-    @Override
-    public void performCoding(ECChunk[] inputChunks, ECChunk[] outputChunks) throws IOException {
+  /**
+   * @param input1 first input for matrix
+   * @param input2 second input for matrix
+   * @return two remaining outputs using matrix
+   */
+  public int[] getPairWiseCouple(int input1, int input2) {}
 
-    }
+  @Override
+  public ECBlock[] getInputBlocks() {
+    return inputBlocks;
+  }
 
-    @Override
-    public void finish() {
+  @Override
+  public ECBlock[] getOutputBlocks() {
+    return outputBlocks;
+  }
 
-    }
+  @Override
+  public void performCoding(ECChunk[] inputChunks, ECChunk[] outputChunks) throws IOException {
+
+  }
+
+  @Override
+  public void finish() {
+
+  }
 }
