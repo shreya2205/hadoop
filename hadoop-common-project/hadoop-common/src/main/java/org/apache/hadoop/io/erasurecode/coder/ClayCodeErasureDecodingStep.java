1ea7456a13014286a3e775169757eecfd2042f08
Chaitnaya Mukka <chaitanya.mvs2007@gmail.com>
2018-06-19 13:06:45 +0530

    Add util functions part1

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/coder/ClayCodeErasureDecodingStep.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/coder/ClayCodeErasureDecodingStep.java
@@ -46,16 +46,36 @@ public class ClayCodeErasureDecodingStep implements ErasureCodingStep {
   }
 
   /**
+   * Get the index of the plane in integer from a base q notation.
    * @param z_vector plane index in vector form
-   * @return z plane index in integer form
+   * @return z plane index
    */
-  public int getZ(int[] z_vector) {}
+  public int getZ(int[] z_vector) {
+    int z =0;
+    int power = 1;
+
+    for(int i=this.t-1; i>=0; --i){
+      z +=z_vector[i]*power;
+      power *= this.q;
+    }
+    return z;
+  }
 
   /**
-   * @param z plane index in integer form
+   * Get the base q notation of the plane
+   * @param z plane index
    * @return plane index in vector form
    */
-  public int[] getZVector(int z) {}
+  public int[] getZVector(int z) {
+    int[] z_vec = new int[this.t];
+
+    for(int i=this.t-1; i>=0; --i){
+      z_vec[i] = z % this.q;
+      z/=this.q;
+    }
+
+    return z_vec;
+  }
 
   /**
    * @param z_vector plane in vector form
@@ -75,20 +95,53 @@ public class ClayCodeErasureDecodingStep implements ErasureCodingStep {
    * @return x+y*q
    */
   public int getIndexInPlane(int x, int y) {
-    return x+q*y;
+    return (x + q*y);
   }
 
 
 
   /**
-   * @param indexInPlane index of the node in the plane (x+y*q)
-   * @param z_vector plane index in vector form
+   * The following are the error types
+   *       -------*----
+   *      |   |   |   |
+   *      --- *-- O---
+   *     |   |   |   |
+   *     + ----------
+   *    |   |   |   |
+   *    O --------- *
+   *
+   *  + indicates both a dot * and a erasure(hole) O
+   *  The above has q = 4 and t = 4.
+   *  (2,0) is an erasure of type 0
+   *  (3,0) is an erasure of type 2
+   *  (1,2) is an erasure of type 1
+   *
+   * @param indexInPlane integer index of the erased node in the plane (x+y*q)
+   * @param z_vector index of the plane in vector form, i.e. base q notation
    * @return return the error type for a node in a plane.
-   * Error types possible are : {0,1,2}
+   * Erasure types possible are : {0,1,2}
    */
-  public int getErrorType(int indexInPlane, int[] z_vector) {}
+  public int getErasureType(int indexInPlane, int[] z_vector) {
+    int[] nodeCoordinates = getNodeCoordinates();
+
+    // there is a hole-dot pair at the given index => type 0
+    if(z_vector[nodeCoordinates[1]] == nodeCoordinates[0])
+        return 0;
+
+    int dotInColumn = getNodeIndex(z_vector[nodeCoordinates[1]], nodeCoordinates[1]);
+
+    // there is a hole dot pair in the same column => type 2
+    for(int i=0; i<this.erasedIndexes.length; ++i){
+      if(this.erasedIndexes[i] == dotInColumn)
+        return 2;
+    }
+
+    return 1;
+
+  }
 
   /**
+   * Convert all the input symbols of the given plane into its decoupled form. We use the rsRawDecoder to achieve this.
    * @param z_vector plane index in vector form
    * @param temp temporary array which stores decoupled values
    * @return decoupled values for all non-null nodes
@@ -97,11 +150,12 @@ public class ClayCodeErasureDecodingStep implements ErasureCodingStep {
 
 
   /**
-   * @param inputs
-   * @param outputs
-   * @return
+   * Get the pairwise couples of the given inputs. Use the pairWiseDecoder to do this.
+   * Notationally inputs are (A,A',B,B') and the outputs array contain the unknown values of the inputs
+   * @param inputs pairwise known couples
+   * @param outputs pairwise couples of the known values
    */
-  public int[] getPairWiseCouple(ByteBuffer[] inputs, ByteBuffer[] outputs) {}
+  public void getPairWiseCouple(ByteBuffer[] inputs, ByteBuffer[] outputs) {}
 
   @Override
   public ECBlock[] getInputBlocks() {
